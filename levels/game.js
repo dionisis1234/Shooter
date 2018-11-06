381ef17a808e0a374d77890deda26f6c66a6c7f8
dionisis1234 <dkal_fas@yahoo.gr>
2018-11-06 09:53:28 +0200

    Update game.js

--- levels/game.js
+++ levels/game.js
@@ -1,4 +1,4 @@
 var game = new Phaser.Game(1100, 800, Phaser.AUTO, 'gameDiv');
     
-       game.state.add('lvl1', vl1);
-game.state.start('vl1');
+       game.state.add('lvl1', lvl1);
+game.state.start('lvl1');
